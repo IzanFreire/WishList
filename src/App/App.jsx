c78ee15125b9a6f4724fb2c73b7cdc431d35710e
IzanFreire <izanfreire@gmail.com>
2024-01-26 13:31:25 +0100

    onNewWish functionality

---
+++ src/App/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './App.css';
+import WishInput from '../WishInput/WishInput';
+import WishList from '../WishList/WishList';
+
+const initialWishes = [
+  {
+    id: 0,
+    text: 'Travel to the moon',
+    completed: false,
+  },
+  {
+    id: 1,
+    text: 'Travel to Barcelona',
+    completed: true,
+  },
+  {
+    id: 2,
+    text: 'Play football',
+    completed: true,
+  },
+];
+
+export default function App() {
+  const [wishes, setWishes] = useState(initialWishes);
+
+  const onNewWish = (newWish) => {
+    setWishes((currentWishes) => [...currentWishes, newWish]);
+  };
+  return (
+    <div className="app">
+      <h1>My Wishlist</h1>
+      <WishInput onNewWish={onNewWish} />
+      <WishList wishes={wishes} />
+    </div>
+  );
+}
---
+++ src/WishInput/WishInput.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export default function WishInput({ onNewWish }) {
+  const [newWish, setNewWish] = useState('');
+
+  const handleKeyUp = (e) => {
+    if (e.key === 'Enter' && newWish !== '') {
+      const newWishObject = {
+        id: Date.now(),
+        text: newWish,
+        Completed: false,
+      };
+      // insertando un nuevo objeto wish en los arrays wish que tiene el padre que es App.jsx
+      onNewWish(newWishObject);
+      setNewWish('');
+    }
+  };
+  return (
+    <fieldset className="wish-input">
+      <legend className="wish-input__label">New Wish:</legend>
+      <input
+        type="text"
+        placeholder="My new Wish"
+        onChange={(e) => setNewWish(e.target.value)}
+        value={newWish}
+        onKeyUp={handleKeyUp}
+      />
+    </fieldset>
+  );
+}
+
+WishInput.propTypes = {
+  onNewWish: PropTypes.func.isRequired,
+};
